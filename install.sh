fdc15b58f8890a831cb8d9d8787ccac8f2b25119
root <root@bionic-amg2>
2020-10-27 23:07:31 +0000

    changes for finalizing demo

--- install.sh
+++ install.sh
@@ -11,8 +11,9 @@
 if [ -z "$2" ]
   then
     echo 'Usage: '$0' <username> <public facing hostname>'
-    echo '  where public facing hostname is the dns name you'll use to access the page.'
+    echo "where public facing hostname is the dns name you'll use to access the page."
     exit
+
 fi
 
 # export for flask (may not be needed for gunicorn)
@@ -34,10 +35,10 @@ ln -s amg/webroot html
 chown -R www-data amg 
 
 # insert provided hostname for api endpoint
-sed -i 's/HTTPHOST/$2/' /var/www/amg/webroot/assets/populate.js
+sed -i 's/HTTPHOST/$2/' /var/www/amg/webroot/assets/js/populate.js
 
 # enable php in sites-available/default
-sed -i '/location.*php/s/#location/location/;/location.*php/,/#}/s/#}/}/;/include.*fastcgi-php/s/#//;/fastcgi_pass.*sock/s/#//' /etc/nginx/sites-available/default
+sed -i '/location.*php/s/#location/location/;/location.*php/,/#}/s/#}/}/;/include.*fastcgi-php/s/#//;/fastcgi_pass.*sock/s/#//;/fastcgi_pass.*sock/s/7.0/7.2/' /etc/nginx/sites-available/default
 
 # add basic auth for admin page
 sed -i '/^server/,/^}/s~^}~\n\tlocation /admin.html {\n\tauth_basic "Authorized access only";\n\tauth_basic_user_file /etc/nginx/.htpasswd;\n\t}\n}~' /etc/nginx/sites-available/default
@@ -55,8 +56,8 @@ After = network.target
 
 [Service]
 WorkingDirectory=/var/www/amg/api
-User=nobody
-Group=nogroup
+User=www-data
+Group=www-data
 ExecStart=/usr/local/bin/gunicorn --bind 0.0.0.0:5000 amg:app -w 3 --access-logfile /tmp/amgapi-access.log --error-logfile /tmp/amgapi-error.log
 ExecReload=/bin/kill -1 $MAINPID
 ExecStop=/bin/kill -15 $MAINPID
@@ -67,7 +68,7 @@ EOF
 
 #setup authentication:
 # add user
-echo -n $1':' >> /etc/nginx/.htpasswd
+echo -n $1: >> /etc/nginx/.htpasswd
 
 # add password (openssl will prompt twice):
 openssl passwd -apr1 >> /etc/nginx/.htpasswd
@@ -79,5 +80,6 @@ systemctl enable gunicorn
 systemctl start gunicorn
 
 # Success!
+echo
 echo "Congratulations! The amg profile generator has been installed."
 echo "Please visit http://"$2"/admin.html to set up your new webpage."
--- webroot/assets/js/populate.js
+++ webroot/assets/js/populate.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
 
   // Adds the specific data to their respective sections
-  $.getJSON('http://HTTPHOST:5000/', function( data ) {
+  $.getJSON('http://amg.elcoz.io:5000/', function( data ) {
     $('title').text(data.name);
     $('#cover_image').css('background', 'url(' + data.cvrimg + ') center / cover no-repeat');
     $('#profile_picture').css('background', 'url(' + data.propic + ') center / cover no-repeat');
@@ -14,7 +14,13 @@ $(document).ready(function() {
     const icons = ['phone', 'facebook', 'twitter', 'instagram', 'email', 'github', 'medium'];
     $.each(icons, function( index, value ) {
     if (isNaN(data[value])) { 
-        $('#contact_details').append('<a href=' + data[value] + '><img id="icons" src=assets/img/icons/' + value + '.png height=32px></img></a>');
+	if (data[value] === 'email') {
+            $('#contact_details').append('<a href=mailto:' + data[value] + '><img id="icons" src=assets/img/icons/' + value + '.png height=32px></img></a>');
+	} else if (data[value] === 'phone') {
+            $('#contact_details').append('<a href=tel:' + data[value] + '><img id="icons" src=assets/img/icons/' + value + '.png height=32px></img></a>');
+	} else {
+            $('#contact_details').append('<a href=tel:' + data[value] + '><img id="icons" src=assets/img/icons/' + value + '.png height=32px></img></a>');
+	};
       };
     });
   });
